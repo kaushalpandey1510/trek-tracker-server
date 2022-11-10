3e24714e9ff0e65d66cf3cf22743b5fb04569992
Kaushal Pandey <kaushalpandey1510@gmail.com>
2022-11-10 18:24:33 -0500

    password encryption and password check

--- src/models/user.js
+++ src/models/user.js
@@ -12,9 +12,49 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save', function() { //we use the 'function' keyword instead of 
-    const user = this;
-    if(!user.Modified('password'))
-})
+
+//encrypting password
+userSchema.pre('save', function () {
+  //we use the 'function' keyword instead of => function, because in arrow fucntion this is refered to the entire file and function keyword this is refered to just that function
+  const user = this;
+  if (!user.Modified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+
+//Password check
+userSchema.methods.comparePassword = function (candiatePassword) {
+  const user = this;
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candiatePassword, user.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (!isMatch) {
+        return reject(false);
+      }
+
+      resolve(true);
+    });
+  });
+};
 
 mongoose.model('User', userSchema);
